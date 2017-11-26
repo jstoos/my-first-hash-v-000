783675fc488e55e3d34007bbd21ec6127d9d9656
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-26 21:36:13 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,11 +29,6 @@ def adding
   # person["hometown"] = "Massena, NY"
   shipping_manifest[muskets] = 2
   shipping_manifest[gun powder] = 4
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
+  shipping_manifest
 
 end
